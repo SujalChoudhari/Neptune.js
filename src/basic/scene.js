6ecf5af8d2187a266df2328ffec5cbc78644c36a
Sujal <sjlchoudhari@gmail.com>
2022-09-25 11:18:45 +0530

    Documented Scene and SceneManager

--- src/basic/scene.js
+++ src/basic/scene.js
@@ -2,7 +2,14 @@ import { Renderable } from "../components/renderable/renderable.js";
 import { Entity } from "./entity.js";
 import { SceneManager } from "./sceneManager.js";
 
-
+/**
+ * Scene is a container for entities. It is used to organize the game objects in the game.
+ * SceneManager is used to load and unload scenes. A scene can be used to only load specific entities.
+ * If any entity is not part of a scene, it will be loaded by default.
+ * @class Scene
+ * @extends Entity
+ * @property {number} id - Id of the scene. This is used to load the scene.
+ */
 export class Scene extends Entity{
     constructor(name,id=0){
         super(name);
@@ -10,10 +17,21 @@ export class Scene extends Entity{
         SceneManager.addScene(this);
     }
 
-    init(){
+    /**
+     * Once a scene is loaded, this function is called.
+     * This function is called by the SceneManager.
+     * @method
+     */
+    Init(){
     }
 
-    draw(ctx){
+    /**
+     * Draw the scene. This will draw all the entities in the scene.
+     * This function is called by the SceneManager.
+     * @param {CanvasRenderingContext2D} ctx - Canvas context to draw on.
+     * 
+     */
+    Draw(ctx){
         this.getComponentsInChildren(Renderable).forEach(renderable => {
             renderable.draw(ctx);
         });
--- src/basic/sceneManager.js
+++ src/basic/sceneManager.js
@@ -1,20 +1,39 @@
 import { Scene } from "./scene.js";
 
+/**
+ * SceneManager is used to load and unload scenes. All scenes will automatically be added to SceneManager.
+ * Only first scene will be loaded by default. To load a scene, use SceneManager.loadScene(sceneId).
+ * @class SceneManager
+ * 
+ */
 export class SceneManager{
     static #scenes =[];
     static #loadedScenes = [];
     static #currentSceneIndex = -1;
     
+    /**
+     * Add a scene to the SceneManager. This is called by the SceneManager.
+     * All Scenes will automatically be added to SceneManager.
+     * @param {Scene} scene - Scene to be added.
+     * @method
+     */
     static addScene(scene){
         if (! scene instanceof Scene) { console.error("scene is not an instance of Scene"); return;}
         SceneManager.#scenes.push(scene);
         if (SceneManager.#scenes.length == 1) SceneManager.loadScene(0);
+
+        SceneManager.#scenes.sort((a,b) => a.id - b.id);
+
     }
 
+    /**
+     * Get a scene by id.
+     * @param {number} id - Id of the scene.
+     */
     static getScene(id){
         return SceneManager.#scenes.find(scene => scene.id == id);
     }
-
+    
     static #unloadScene(id){
         let scene = SceneManager.getScene(id);
         if(scene){
@@ -22,25 +41,48 @@ export class SceneManager{
         }
     }
 
+    /**
+     * Load a scene by id. This will unload the current scene and load the new scene.
+     * @param {number} id - Id of the scene to be loaded.
+     * @method
+     */
     static loadScene(id){
         if(id == SceneManager.#currentSceneIndex){console.warn("Scene already loaded"); return;}
         SceneManager.#unloadScene(this.#currentSceneIndex);
         SceneManager.#currentSceneIndex = id;
         SceneManager.#loadedScenes.push(SceneManager.getScene(id));
+        SceneManager.#scenes[id].Init();
     }
 
-
+    /**
+     * Load a new scene in additive mode. This will not unload the current scene.
+     * But currentSceneIndex will not set to the new scene.
+     * @param {number} id - Id of the scene to be loaded.
+     */
     static loadSceneAdditive(id){
         SceneManager.#loadedScenes.push(SceneManager.getScene(id));
+        SceneManager.#scenes[id].Init();
     }
 
+    /**
+     * Unload all added scenes. This will not unload the current scene.
+     * @method
+     */
     static unloadAllAdditiveScenes(){
         SceneManager.#loadedScenes.splice(1,SceneManager.#loadedScenes.length-1);
     }
 
+    /**
+     * Draw all loaded scenes. This will draw all the entities in the scene.
+     * This method is called automatically by the game loop.
+     * Order of drawing is based on the order of the scenes in the SceneManager or the order of scene Ids.
+     * @param {CanvasRenderingContext2D} ctx - Canvas context to draw on.
+     * @method
+     * 
+     */
     static Draw(ctx){
         SceneManager.#loadedScenes.forEach(scene => {
-            scene.draw(ctx);
+            scene.Draw(ctx);
         });
     }
     
